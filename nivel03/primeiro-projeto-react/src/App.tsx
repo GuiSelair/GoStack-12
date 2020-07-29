aa3575d624ed4bb3b293fb63582ee47e910f7485
guiselair <guilherme.lima1997@hotmail.com>
2020-07-28 22:14:25 -0300

    Update-Lesson06

--- nivel03/primeiro-projeto-react/src/App.tsx
+++ nivel03/primeiro-projeto-react/src/App.tsx
@@ -2,6 +2,14 @@ import React from 'react';
 import {BrowserRouter} from 'react-router-dom';
 import Routes from './routes';
 
-const App: React.FC = () => <BrowserRouter><Routes /></BrowserRouter>
+import GlobalStyles from './styles/global';
+
+const App: React.FC = () =>
+  <>
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+    <GlobalStyles />
+  </>
 
 export default App;
--- nivel03/primeiro-projeto-react/src/pages/Dashboard/index.tsx
+++ nivel03/primeiro-projeto-react/src/pages/Dashboard/index.tsx
@@ -1,7 +1,68 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
+import { FiChevronRight } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo-github-explorer.svg';
+import api from '../../services/api';
+
+import { Title, Form, Repositories} from "./styles";
+
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  }
+}
+
 
 const Dashboard: React.FC = () => {
-    return <h1>Dashboard</h1>
+  const [newRepo, setNewRepo] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+
+  const handleAddRepository = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const response = await api.get<Repository>(`repos/${newRepo}`)
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
+    setNewRepo('');
+  };
+
+  return (
+    <>
+      <img src={logoImg} alt="Logo Github_Explorer"/>
+      <Title>Explore repositórios</Title>
+
+      <Form onSubmit={handleAddRepository}>
+        <input
+          type="text"
+          placeholder='Digite o nome do repositório'
+          value = {newRepo}
+          onChange = {(event) => setNewRepo(event?.target.value)}
+
+        />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        {repositories.map(repository => (
+          <a key={ repository.full_name } href="#">
+            <img src={ repository.owner.avatar_url } alt={ repository.owner.login }/>
+            <div>
+              <strong>{ repository.full_name }</strong>
+              <p>{ repository.description }</p>
+            </div>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
+
+
+      </Repositories>
+    </>
+  )
 }
 
 export default Dashboard
