52f018461861b1e2517845a9d1e99fc2323bde54
guiselair <guilherme.lima1997@hotmail.com>
2020-08-12 22:02:44 -0300

    Finished Input components, Add Tooltip components

--- nivel03/gobarber-web/src/pages/SignUp/index.tsx
+++ nivel03/gobarber-web/src/pages/SignUp/index.tsx
@@ -1,7 +1,10 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useRef } from 'react';
 import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
 import { Form } from '@unform/web';
+import { FormHandles } from '@unform/core';
 import * as Yup from 'yup';
+
+import validationsErrors from '../../Utils/getValidationErrors';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
@@ -11,8 +14,10 @@ import { Container, Content, Background } from './styles';
 import logo from '../../assets/logo.svg';
 
 const SignUp: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
   const handleSubmit = useCallback(async (data: object) => {
     try {
+      formRef.current?.setErrors({});
       const schema = Yup.object().shape({
         name: Yup.string().required('Nome obrigatório'),
         email: Yup.string()
@@ -25,6 +30,8 @@ const SignUp: React.FC = () => {
       });
     } catch (error) {
       console.log(error);
+      const errors = validationsErrors(error);
+      formRef.current?.setErrors(errors);
     }
   }, []);
 
@@ -33,7 +40,7 @@ const SignUp: React.FC = () => {
       <Background />
       <Content>
         <img src={logo} alt="GoBarber" />
-        <Form onSubmit={handleSubmit}>
+        <Form ref={formRef} onSubmit={handleSubmit}>
           <h1>Faça seu cadastro</h1>
           <Input name="name" icon={FiUser} type="text" placeholder="Nome" />
           <Input name="email" icon={FiMail} type="email" placeholder="E-mail" />
