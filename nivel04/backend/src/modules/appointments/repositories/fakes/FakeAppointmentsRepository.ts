7e3027ae855ff5af1a2c7d78c30577611f1e3937
Guilherme Selair <guilherme.lima1997@hotmail.com>
2020-09-24 16:51:10 -0300

    Feat: Add first test :D

---
+++ nivel04/backend/src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
@@ -0,0 +1,31 @@
+import { uuid } from 'uuidv4';
+
+import IAppointmetsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
+import ICreateAppointmentsDTO from '@modules/appointments/dtos/ICreateAppointmentsDTO';
+
+import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
+
+class AppointmentsRepository implements IAppointmetsRepository {
+  private appointments: Appointment[] = [];
+
+  public async findByDate(date: Date): Promise<Appointment | undefined> {
+    const findAppointment = this.appointments.find(
+      appointment => appointment.date === date,
+    );
+    return findAppointment;
+  }
+
+  public async create({
+    provider_id,
+    date,
+  }: ICreateAppointmentsDTO): Promise<Appointment> {
+    const appointments = new Appointment();
+
+    Object.assign(appointments, { id: uuid(), date, provider_id });
+    this.appointments.push(appointments);
+
+    return appointments;
+  }
+}
+
+export default AppointmentsRepository;
---
+++ nivel04/backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -0,0 +1,18 @@
+import CreateAppointmentService from './CreateAppointmentService';
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+
+describe('CreateAppointment', () => {
+  it('should be able a new appointment', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointmentService = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+    const appointment = await createAppointmentService.execute({
+      date: new Date(),
+      provider_id: '4556',
+    });
+
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('4556');
+  });
+});
